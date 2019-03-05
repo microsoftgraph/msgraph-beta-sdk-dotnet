2bdde105e7fae656a358d94e3029e0d5dd8c16c6
Michael Mainer <mmainer@microsoft.com>
2019-03-04 18:28:56 -0800

    Updated library name

--- src/Microsoft.Graph/Properties/AssemblyInfo.cs
+++ src/Microsoft.Graph/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft Graph Client Library")]
-[assembly: AssemblyDescription("This package includes the client libraries for connecting your app to the Microsoft Graph. Microsoft Graph enables your app to interact with data entities and their relationships from many Microsoft cloud services with a unified developer experience. Learn more about Microsoft Graph at http://graph.microsoft.io.")]
+[assembly: AssemblyTitle("Microsoft Graph Beta Client Library")]
+[assembly: AssemblyDescription("This package includes the beta client library for connecting your app to the Microsoft Graph beta endpoint. Microsoft Graph enables your app to interact with data entities and their relationships from many Microsoft cloud services with a unified developer experience. Learn more about Microsoft Graph at http://graph.microsoft.io.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("Microsoft.Graph")]
+[assembly: AssemblyProduct("Microsoft.Graph.Beta")]
 [assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
