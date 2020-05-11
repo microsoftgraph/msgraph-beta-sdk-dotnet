78646c541e482e56a17750fc4ed2126744062955
Vincent Biret <vincentbiret@hotmail.com>
2020-05-11 10:20:18 -0400

    - adds partial class interface implementation

--- src/Microsoft.Graph/Models/Extensions/Subscription.cs
+++ src/Microsoft.Graph/Models/Extensions/Subscription.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-
-namespace Microsoft.Graph
+﻿namespace Microsoft.Graph
 {
-    public partial class Subscription
+    public partial class Subscription : IEncryptableSubscription
     {
-        /// <summary>
-        /// Adds the encryption certificate information for change notifications with resource data to the subscription creation information.
-        /// </summary>
-        /// <param name="certificate">Certificate to use for encryption</param>
-        public void AddEncryptionCertificate(X509Certificate2 certificate)
-        {
-            EncryptionCertificate = Convert.ToBase64String(certificate.Export(X509ContentType.SerializedCert));
-            IncludeResourceData = true;
-        }
     }
 }
