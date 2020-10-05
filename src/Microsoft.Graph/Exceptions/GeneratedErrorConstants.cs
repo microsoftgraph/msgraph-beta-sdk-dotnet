a104cea87c85fbd6d8bbab470bab38ba2364cf6c
Michael Mainer <MIchaelMainer@users.noreply.github.com>
2020-10-05 13:56:53 -0700

    Update GeneratedErrorConstants.cs
    Related to https://github.com/microsoftgraph/msgraph-sdk-dotnet/pull/803

--- src/Microsoft.Graph/Exceptions/GeneratedErrorConstants.cs
+++ src/Microsoft.Graph/Exceptions/GeneratedErrorConstants.cs
@@ -4,16 +4,16 @@
 
 namespace Microsoft.Graph
 {
-    internal static class GeneratedErrorConstants
+    public static class GeneratedErrorConstants
     {
-        internal static class Codes
+        public static class Codes
         {
-            internal static string NotAllowed = "notAllowed";
+            public static string NotAllowed = "notAllowed";
         }
 
-        internal static class Messages
+        public static class Messages
         {
-            internal static string ResponseObjectUsedForUpdate = "Do not use objects returned in a response for updating an object in Microsoft Graph. " +
+            public static string ResponseObjectUsedForUpdate = "Do not use objects returned in a response for updating an object in Microsoft Graph. " +
                                                                  "Create a new {0} object and only set the updated properties on it.";
         }
     }
