8aa24aaa8da10368f6d57dddfe7418aa6211d0ae
Andrew Omondi <andrew.omondi@microsoft.com>
2021-02-19 14:59:20 +0300

    Add constants

--- src/Microsoft.Graph/Constants.cs
+++ src/Microsoft.Graph/Constants.cs
@@ -1,4 +1,4 @@
-// ------------------------------------------------------------------------------
+﻿// ------------------------------------------------------------------------------
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------
 
@@ -59,6 +59,22 @@ namespace Microsoft.Graph
             /// The Status Code string
             /// </summary>
             public const string StatusCode = "statusCode";
+        }
+
+        /// <summary>
+        /// Constants used to specify content types
+        /// </summary>
+        public static class ContentTypes
+        {
+            /// <summary>
+            /// The deltaLink annotations string.
+            /// </summary>
+            public const string JsonContentType = "application/json";
+
+            /// <summary>
+            /// The deltaLink annotations string.
+            /// </summary>
+            public const string StreamContentType = "application/octet-stream";
         }
     }
 }
