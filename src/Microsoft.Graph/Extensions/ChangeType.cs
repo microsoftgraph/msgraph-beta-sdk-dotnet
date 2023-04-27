e931f846ab3d840fa2c55d08630b15e620b64874
Andrew Omondi <andrew.omondi@microsoft.com>
2023-04-27 14:36:04 +0300

    Adds change notification types

---
+++ src/Microsoft.Graph/Extensions/ChangeType.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+using System;
+namespace Microsoft.Graph.Beta.Models {
+    public enum ChangeType {
+        [EnumMember(Value = "created")]
+        Created,
+        [EnumMember(Value = "updated")]
+        Updated,
+        [EnumMember(Value = "deleted")]
+        Deleted,
+    }
+}
