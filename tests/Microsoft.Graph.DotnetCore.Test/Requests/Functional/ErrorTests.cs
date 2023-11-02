6a5728f6c009b04b8d30f414571419396ed6340d
Andrew Omondi (from Dev Box) <anomondi@microsoft.com>
2023-11-02 09:07:49 +0300

    Fixes failing tests

--- tests/Microsoft.Graph.DotnetCore.Test/Requests/Functional/ErrorTests.cs
+++ tests/Microsoft.Graph.DotnetCore.Test/Requests/Functional/ErrorTests.cs
@@ -11,7 +11,7 @@
             // To get a throttling error, I mocked up a 429 response in a text file and turned on the Fiddler
             // autoresponder to return the text file as the response envelope. The autoresponder for this 
             // scenario responds to EXACT:https://graph.microsoft.com/v1.0/groups/036bd54c-c6e5-43eb-b8b5-03e019e75bd1
-            ServiceException e = await Assert.ThrowsAsync<ServiceException>(async () => await graphClient.Groups["036bd54c-c6e5-43eb-b8b5-03e019e75bd1"].GetAsync(requestConfiguration => requestConfiguration.Headers.Add("client-request-id", "dddddddd-dddd-dddd-dddd-dddddddddddd")));
+            ServiceException e = await Assert.ThrowsAsync<ServiceException>(() => graphClient.Groups["036bd54c-c6e5-43eb-b8b5-03e019e75bd1"].GetAsync(requestConfiguration => requestConfiguration.Headers.Add("client-request-id", "dddddddd-dddd-dddd-dddd-dddddddddddd")));
 
             Assert.Contains("tooManyRetries", e.Message);
         }
