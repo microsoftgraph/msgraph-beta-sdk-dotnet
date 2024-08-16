5cbc0dc6e6725feb3a9831586faaeb00ec0df11a
Andrew Omondi <anomondi@microsoft.com>
2024-08-16 12:21:29 +0300

    chore: branch alignement to use main branch

--- scripts/GetLatestCommitSHA.ps1
+++ scripts/GetLatestCommitSHA.ps1
@@ -17,13 +17,13 @@
     Specifies the name of the repository.
 
 .Parameter branchName
-    Specifies the target branch name. The default value is 'master'.
+    Specifies the target branch name. The default value is 'main'.
 #>
 
 Param(
     [string]$owner,
     [string]$repo,
-    [string]$branchName = "master"
+    [string]$branchName = "main"
 )
 
 if ([string]::IsNullOrEmpty($owner)) {
